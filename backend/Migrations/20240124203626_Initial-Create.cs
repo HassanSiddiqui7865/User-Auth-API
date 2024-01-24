b026d33a80d8c9d0d353329522f0a0337a287b35
Sumeed Iqbal <sumeed.iqbal@octdaily.com>
2024-01-25 02:58:54 +0500

    fixes on errors

---
+++ backend/Migrations/20240124203626_Initial-Create.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    bookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    bookName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    bookAuthor = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    bookDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    imgUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.bookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Projectshortname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Projectfullname = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "USERS",
+                columns: table => new
+                {
+                    userId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    username = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    pass = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    userrole = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USERS", x => x.userId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "USERS");
+        }
+    }
+}
